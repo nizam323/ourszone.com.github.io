5eb5fa57f6ae8ec0be6ea644185c6fd8e0c9fa52
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-11-30 21:52:58 +0500

    working on home posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -10,8 +10,6 @@ function Posts() {
 
     const [data, setData] = useState([]);
     const [dataUsersPosts, setDataUsersPosts] = useState([]);
-    const [s, set] = useState([]);
-    let allUsersPosts = [];
 
     useEffect(() => {
         let fn = onAuthStateChanged(auth, (user) => {
@@ -28,18 +26,15 @@ function Posts() {
     }, []);
 
     useEffect(() => {
-
-        setDataUsersPosts(data != "" ? Object.values(data.map((i) => i.posts)) : "nahi aya")
-        set(allUsersPosts != "" ? Object.values(allUsersPosts) : "nahi aya")
-        
+        const allPosts = data
+            ? data.flatMap((items) =>
+                items.posts ? Object.values(items.posts) : []
+            )
+            : [];
+        setDataUsersPosts(allPosts);
     }, [data]);
-    
-    allUsersPosts.push(dataUsersPosts)
 
-    console.log("data", data);
     console.log("dataUsersPosts", dataUsersPosts);
-    console.log("allUsersPosts", allUsersPosts);
-    console.log("set", s);
 
     let date = new Date();
     const initialPosts = [
@@ -87,98 +82,135 @@ function Posts() {
     }
 
     return (
-
-        // <div className="posts py-0 px-3" style={{ backgroundColor: "#000000" }}>
-        //     {posts.length <= 0 ? (
-        //         <p style={{ color: "gray" }} className="text-center lead fw-normal mb-0">
-        //             Your Posts Appear Here
-        //         </p>
-        //     ) : (
-        //         posts.map((post) => (
-        //             <div
-        //                 key={post.id}
-        //                 className="card"
-        //                 style={{
-        //                     width: "96%",
-        //                     margin: "15px auto 0",
-        //                     border: "1px solid white",
-        //                     borderRadius: ".25rem",
-        //                 }}
-        //             >
-        //                 <span className="d-flex">
-        //                     <img
-        //                         alt="..."
-        //                         src={post.url}
-        //                         style={{
-        //                             width: "50px",
-        //                             height: "50px",
-        //                             borderRadius: "50%",
-        //                             border: "1px solid",
-        //                             margin: "10px 15px 0 10px",
-        //                         }}
-        //                     />
-        //                     <div className="d-flex flex-column" style={{ marginTop: "10px" }}>
-        //                         <h4>{post.profileName}</h4>
-        //                         <pre>
-        //                             {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}  ${post.hr <= 9 ? "0" + post.hr : post.hr
-        //                                 }:${post.min <= 9 ? "0" + post.min : post.min}:${post.sec <= 9 ? "0" + post.sec : post.sec
-        //                                 }`}
-        //                         </pre>
-        //                     </div>
-        //                 </span>
-        //                 <div className="card-body">
-        //                     <p className="card-text">{post.text}</p>
-        //                 </div>
-        //                 {post.url && <img src={post.url} className="card-img-top" alt={`Post ${post.id}`} />}
-        //                 <div className="d-flex justify-content-around m-3">
-        //                     <div className={post.likes == false ? "like-btn like" : "like"} onClick={() => handleLike(post.id)}>
-        //                         Like {post.likesCount > 0 && post.likesCount}
-        //                     </div>
-        //                     <div className={post.showComments ? "com-btn like" : "like"} onClick={() => toggleComments(post.id)}>
-        //                         Comments
-        //                     </div>
-        //                 </div>
-        //                 {
-        //                     post.showComments && (
-        //                         <div className="comments-box-con">
-        //                             <div className="comments-box">
-        //                                 <div className="close-comments" onClick={() => toggleComments(post.id)}>X</div>
-        //                                 <div className="comments-con">
-        //                                     <div className="com-img"><img src={post.url} width="35" height="35" /></div>
-        //                                     <div style={{ marginTop: "10px" }}>
-        //                                         <h4>{post.profileName}</h4>
-        //                                         <pre>
-        //                                             {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}  ${post.hr <= 9 ? "0" + post.hr : post.hr
-        //                                                 }:${post.min <= 9 ? "0" + post.min : post.min}:${post.sec <= 9 ? "0" + post.sec : post.sec
-        //                                                 }`}
-        //                                         </pre>
-        //                                         <div style={{ backgroundColor: "white", padding: "2px" }}>Comment Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi culpa error veritatis delectus aliquam dolores. Laborum minus itaque magni iusto beatae accusamus corporis accusantium dolore ratione, veniam, alias voluptates. Section for Post {post.id}</div>
-        //                                     </div>
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     )
-        //                 }
-        //             </div>
-        //         ))
-        //     )
-        //     }
-        // </div >
-
-
+        <>
+            <div className="posts py-0 px-3" style={{ backgroundColor: "#000000" }}>
+                {posts.length <= 0 ? (
+                    <p style={{ color: "gray" }} className="text-center lead fw-normal mb-0">
+                        Your Posts Appear Here
+                    </p>
+                ) : (
+                    posts.map((post) => (
+                        <div
+                            key={post.id}
+                            className="card"
+                            style={{
+                                width: "96%",
+                                margin: "15px auto 0",
+                                border: "1px solid white",
+                                borderRadius: ".25rem",
+                            }}
+                        >
+                            <span className="d-flex">
+                                <img
+                                    alt="..."
+                                    src={post.url}
+                                    style={{
+                                        width: "50px",
+                                        height: "50px",
+                                        borderRadius: "50%",
+                                        border: "1px solid",
+                                        margin: "10px 15px 0 10px",
+                                    }}
+                                />
+                                <div className="d-flex flex-column" style={{ marginTop: "10px" }}>
+                                    <h4>{post.profileName}</h4>
+                                    <pre>
+                                        {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}  ${post.hr <= 9 ? "0" + post.hr : post.hr
+                                            }:${post.min <= 9 ? "0" + post.min : post.min}:${post.sec <= 9 ? "0" + post.sec : post.sec
+                                            }`}
+                                    </pre>
+                                </div>
+                            </span>
+                            <div className="card-body">
+                                <p className="card-text">{post.text}</p>
+                            </div>
+                            {post.url && <img src={post.url} className="card-img-top" alt={`Post ${post.id}`} />}
+                            <div className="d-flex justify-content-around m-3">
+                                <div className={post.likes == false ? "like-btn like" : "like"} onClick={() => handleLike(post.id)}>
+                                    Like {post.likesCount > 0 && post.likesCount}
+                                </div>
+                                <div className={post.showComments ? "com-btn like" : "like"} onClick={() => toggleComments(post.id)}>
+                                    Comments
+                                </div>
+                            </div>
+                            {
+                                post.showComments && (
+                                    <div className="comments-box-con">
+                                        <div className="comments-box">
+                                            <div className="close-comments" onClick={() => toggleComments(post.id)}>X</div>
+                                            <div className="comments-con">
+                                                <div className="com-img"><img src={post.url} width="35" height="35" /></div>
+                                                <div style={{ marginTop: "10px" }}>
+                                                    <h4>{post.profileName}</h4>
+                                                    <pre>
+                                                        {`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}  ${post.hr <= 9 ? "0" + post.hr : post.hr
+                                                            }:${post.min <= 9 ? "0" + post.min : post.min}:${post.sec <= 9 ? "0" + post.sec : post.sec
+                                                            }`}
+                                                    </pre>
+                                                    <div style={{ backgroundColor: "white", padding: "2px" }}>Comment Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi culpa error veritatis delectus aliquam dolores. Laborum minus itaque magni iusto beatae accusamus corporis accusantium dolore ratione, veniam, alias voluptates. Section for Post {post.id}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    ))
+                )
+                }
+            
 
 
-        <p>
-            {
-                // data.length < 0  ? 
-                allUsersPosts.map((i, index) => {
-                    // <p key={index}>lol</p>
-                    return  <h1>s{i.hr}</h1>
-                           
-                })
-            }
-        </p>
-
+            <div>
+                {dataUsersPosts.map((post, index) => (
+                    <div
+                        key={index}
+                        className="card"
+                        style={{
+                            width: "96%",
+                            margin: "15px auto 0",
+                            border: "1px solid white",
+                            borderRadius: ".25rem",
+                        }}
+                    >
+                        <span className="d-flex">
+                            <img
+                                alt="Profile"
+                                src={post.url || ""}
+                                style={{
+                                    width: "50px",
+                                    height: "50px",
+                                    borderRadius: "50%",
+                                    border: "1px solid",
+                                    margin: "10px 15px 0 10px",
+                                }}
+                            />
+                            <div className="d-flex flex-column" style={{ marginTop: "10px" }}>
+                                <h4>{post.profileName || "Unknown User"}</h4>
+                                <pre>{`${post.date}/${post.month}/${post.hr} | ${post.hr}hr ${post.min}min ${post.sec}sec`}</pre>
+                            </div>
+                        </span>
+                        <div className="card-body">
+                            <p className="card-text">{post.postTitle || "No content available"}</p>
+                        </div>
+                        {<img src={post.postPicUrl} className="card-img-top" alt={`Post`} />}
+                        <div className="d-flex justify-content-around m-3">
+                            <div
+                                className={post.likes ? "like-btn like" : "like"}
+                            >
+                                Like
+                            </div>
+                            <div
+                                className={post.showComments ? "com-btn like" : "like"}
+                            >
+                                Comments
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div >
+            </div>
+        </>
     );
 }
 
